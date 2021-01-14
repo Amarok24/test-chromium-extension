03d645a8abdd1ddeed775a451f75b5c33b5114e1
Jan Pražák <jp.amarok@yahoo.com>
2021-01-14 17:26:15 +0100

    Add files via upload

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,1 +1,38 @@
 
+/*
+https://docs.microsoft.com/en-gb/microsoft-edge/extensions-chromium/getting-started/
+
+https://docs.microsoft.com/en-gb/microsoft-edge/extensions-chromium/developer-guide/api-support
+
+https://developer.chrome.com/docs/extensions/mv3/messaging/
+https://developer.chrome.com/docs/extensions/reference/runtime/
+https://developer.chrome.com/docs/extensions/reference/tabs/
+
+*/
+
+function MyListener(request, sender, sendResponse)
+{
+	$("head").prepend(
+		`<style>
+		  .slide-image {
+			  height: auto;
+			  width: 100vw;
+			}
+		 </style> `
+	);
+
+	$("body").prepend(
+		`<img src="${request.url}" id="${request.imageDivId}" class="slide-image" /> `
+	);
+
+	$(`#${request.imageDivId}`).click( function()
+	{
+		$(`#${request.imageDivId}`).remove(`#${request.imageDivId}`);
+	});
+
+	sendResponse({ farewell: "goodbye" });
+}
+
+
+// Listener gets fired when a message is sent from either an extension process (by sendMessage) or a content script (by tabs.sendMessage).
+chrome.runtime.onMessage.addListener(MyListener);
